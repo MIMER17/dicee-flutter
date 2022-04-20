df9639f67235f423084fb1f2204baadc02f6c2a7
mihir <mihirshetty9@gmail.com>
2022-04-20 23:19:26 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -18,6 +19,40 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    var leftDiceNumber = 3;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: ElevatedButton(
+                onPressed: (){
+                  print('i clicked on the left button');
+                },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                  ),
+                  child: Image.asset('images/dice$leftDiceNumber.png')
+              ),
+          ),
+          Expanded(
+              child: TextButton(
+                onPressed: (){
+                  print('clicked on the right button');
+                },
+                  child: Image.asset('images/dice2.png')
+              ),
+          ),
+        ],
+      ),
+    );
   }
 }
+// Expanded(
+// child: Padding(
+// padding: const EdgeInsets.all(8.0),
+// child: TextButton(
+// child: Image.asset("images/dice1.png"),
+// ),
+// ),
+// ),
